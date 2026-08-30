230ec19993f23316044534d4d423d00d769290db
salithach <salithachathuranga94@gmail.com>
2026-08-30 20:21:36 +0530

    Implement appointments edit and delete APIs

---
+++ src/main/java/com/salonhq/server/exception/AppointmentOperationException.java
@@ -0,0 +1,8 @@
+package com.salonhq.server.exception;
+
+public class AppointmentOperationException extends RuntimeException {
+    public AppointmentOperationException(String message) {
+        super(message);
+    }
+}
+
